3717327c1b2a24672ec1f702e9955679dc86f44b
matheus <matheus.britto@gmail.com>
2022-08-09 20:41:26 -0300

    Spotify V1

--- Scripts/creator.py
+++ Scripts/creator.py
@@ -34,8 +34,8 @@ print(Fore.RESET + 'Your country: ' + Fore.LIGHTBLUE_EX + user_ct)
 time.sleep(1)
 print()
 
-print(Fore.RESET + 'For this to work I need some sort of input from you! \nI will need a Artist, a Track,'
-                   ' Energy, Mood.')
+print(Fore.RESET + 'For this to work I will need some inputs from you! \nI will need a Artist, a Track,'
+                   ' an Energy and a Mood.')
 print()
 print('But no worries I will explain what everything means')
 print()
@@ -44,7 +44,6 @@ print(Fore.LIGHTBLUE_EX + 'Energy parameter: ' + Fore.RESET + 'Represents a perc
                                                               'activity.'
                                                               '\nTypically, energetic tracks feel fast, loud, '
                                                               'and noisy.')
-
 
 # Indexes
 ''' energy = ('Dull', 'Detached', 'Calm', 'Emphatic', 'Excited')
@@ -57,7 +56,7 @@ print()
 time.sleep(1)
 print(Fore.LIGHTBLUE_EX + 'Mood parameter: ' + Fore.RESET + 'Represents the general feel of the song, a higher number '
                                                             'sound more positive (e.g. happy, '
-      '\ncheerful, euphoric), while tracks with a lower number sound more negative (e.g. sad, depressed, angry)')
+                                                            '\ncheerful, euphoric), while tracks with a lower number sound more negative (e.g. sad, depressed, angry)')
 print()
 time.sleep(1)
 print('Please read carefully the parameters descriptions above to get the best results.')
@@ -65,92 +64,98 @@ print('Now lets start!')
 print()
 time.sleep(1)
 
-while True:
 
-    # Inputs
+def creator():
+    while True:
 
-    artist_input = input(Fore.LIGHTRED_EX + 'Enter the Artist Name: ')
-    track_input = input(Fore.LIGHTRED_EX + 'Enter the Track Name: ')
-    target_energy = input(Fore.LIGHTRED_EX + 'Enter the Target Energy: ')
-    target_mood = input(Fore.LIGHTRED_EX + 'Enter the Target Mood: ')
-    print()
-    print(Fore.RESET + 'All right, just a minute and I will be back with the results')
+        # Inputs
 
-    # Setting up base queries
+        artist_input = input(Fore.LIGHTRED_EX + 'Enter the Artist Name: ')
+        track_input = input(Fore.LIGHTRED_EX + 'Enter the Track Name: ')
+        target_energy = input(Fore.LIGHTRED_EX + 'Enter the Target Energy: ')
+        target_mood = input(Fore.LIGHTRED_EX + 'Enter the Target Mood: ')
+        print()
+        print(Fore.RESET + 'All right, just a minute and I will be back with the results')
 
-    artist = sp.artist(artist_input)
+        # Setting up base queries
 
-    artist_id = ''.join(artist['id'])
-    genres = ''.join(artist['genres'])
+        artist = sp.artist(artist_input)
 
-    track = sp.track(track_input)
+        artist_id = ''.join(artist['id'])
+        genres = ''.join(artist['genres'])
 
-    track_id = ''.join(track['id'])
+        track = sp.track(track_input)
 
-    seed_artist = artist_id
+        track_id = ''.join(track['id'])
 
-    seed_track = track_id
+        seed_artist = artist_id
 
-    seed_genre = genres
+        seed_track = track_id
 
-    # Query
-    recommendations = sp.recommendations(seed_artist,
-                                         seed_track,
-                                         seed_genre,
-                                         target_energy,
-                                         target_mood)
-    print()
-    print(Fore.LIGHTYELLOW_EX + f"{recommendations['recommendations'].to_string(index=False)}")
-    print()
-    print(Fore.RESET + 'Cool ' + f'{Fore.LIGHTBLUE_EX + username}' + Fore.RESET + '!!')
-    print()
-    time.sleep(1)
-    print('Now I need just a couple more things to get your playlist ready!')
-    print()
-    time.sleep(1)
-    pl_name = input(Fore.LIGHTRED_EX + 'Could you name your playlist? ')
-    time.sleep(1)
-    print()
-    pl_disc = input(Fore.LIGHTRED_EX + 'Now, do you want to give some description? (optional) ')
-    time.sleep(1)
-    print()
-    print(Fore.RESET + 'Cool, be right back')
+        seed_genre = genres
 
-    # Setting Up Play list
-
-    playlist = sp.create_list(f'{user_id}', f'{pl_name}', f'{pl_disc}')
-
-    playlist_id = playlist['id']
-    playlist_url = playlist['url']
-
-    # Adding Tracks
-
-    recommendations_ids = recommendations['id']
-
-    add_songs = sp.add_songs(playlist_id, recommendations_ids)
-
-    print()
-    print('Yo, just finished, here is your playlist info')
-    print()
-    print(Fore.RESET + 'Playlist Name: ' + Fore.LIGHTYELLOW_EX + f'{pl_name}',
-          Fore.RESET + '\nID: ' + Fore.LIGHTYELLOW_EX + f'{playlist_id}',
-          Fore.RESET + '\nURL: ' + Fore.LIGHTYELLOW_EX + f'{playlist_url}')
-    print()
-    time.sleep(1)
-    print(Fore.RESET + 'Hope you enjoy your new playlist!!')
-    print('Just one more thing, do you want do it again?')
-    print()
-    time.sleep(1)
-    print(Fore.LIGHTBLUE_EX + '1' + Fore.RESET + '- Yes')
-    print(Fore.LIGHTBLUE_EX + '2' + Fore.RESET + '- No')
-    print()
-    response = input(Fore.LIGHTRED_EX + 'Choose a number: ')
-    if response == '1':
-        print(Fore.RESET + 'Nice, let clear your last results')
+        # Query
+        recommendations = sp.recommendations(seed_artist,
+                                             seed_track,
+                                             seed_genre,
+                                             target_energy,
+                                             target_mood)
+        print()
+        print(Fore.LIGHTYELLOW_EX + f"{recommendations['recommendations'].to_string(index=False)}")
+        print()
+        print(Fore.RESET + 'Cool ' + f'{Fore.LIGHTBLUE_EX + username}' + Fore.RESET + '!!')
         print()
         time.sleep(1)
-        continue
+        print('Now I need just a couple more things to get your playlist ready!')
+        print()
+        time.sleep(1)
+        pl_name = input(Fore.LIGHTRED_EX + 'Could you name your playlist? ')
+        time.sleep(1)
+        print()
+        pl_disc = input(Fore.LIGHTRED_EX + 'Now, do you want to give some description? (optional) ')
+        time.sleep(1)
+        print()
+        print(Fore.RESET + 'Cool, be right back')
 
-    else:
-        print('No worries, we do it another time!')
-        break
+        # Setting Up Play list
+
+        playlist = sp.create_list(f'{user_id}', f'{pl_name}', f'{pl_disc}')
+
+        playlist_id = playlist['id']
+        playlist_url = playlist['url']
+
+        # Adding Tracks
+
+        recommendations_ids = recommendations['id']
+
+        add_songs = sp.add_songs(playlist_id, recommendations_ids)
+
+        print()
+        print('Yo, just finished, here is your playlist info')
+        print()
+        print(Fore.RESET + 'Playlist Name: ' + Fore.LIGHTYELLOW_EX + f'{pl_name}',
+              Fore.RESET + '\nID: ' + Fore.LIGHTYELLOW_EX + f'{playlist_id}',
+              Fore.RESET + '\nURL: ' + Fore.LIGHTYELLOW_EX + f'{playlist_url}')
+        print()
+        time.sleep(1)
+        print(Fore.RESET + 'Hope you enjoy your new playlist!!')
+        print('Just one more thing, do you want do it again?')
+        print()
+        time.sleep(1)
+        print(Fore.LIGHTBLUE_EX + '1' + Fore.RESET + '- Yes')
+        print(Fore.LIGHTBLUE_EX + '2' + Fore.RESET + '- No')
+        print()
+        response = input(Fore.LIGHTRED_EX + 'Choose a number: ')
+        if response == '1':
+            print(Fore.RESET + 'Nice, let clear your last results')
+            print()
+            time.sleep(1)
+            continue
+
+        else:
+            print('No worries, we do it another time!')
+            break
+
+
+if __name__ == '__main__':
+    creator()
